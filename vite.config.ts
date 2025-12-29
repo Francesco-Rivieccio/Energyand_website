8d98471880f1a21f3f12e1df6e2cb29763bc5007
Francesco Rivieccio <frankierivis@gmail.com>
2025-12-29 13:16:42 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Energyand_website/", //
-
   server: {
     host: "::",
     port: 8080,
+    historyApiFallback: true, // 👈 Necessary for React Router
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
